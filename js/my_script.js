c8585e031ee0caeb90941109a0f14e3c4c99dfcb
ayaan <ayaanh221@gmail.com>
2019-11-15 12:31:23 +0000

    first commit

--- js/my_script.js
+++ js/my_script.js
@@ -1,3 +1,27 @@
-$(document).ready(function(){
-    $("h2").css("text")
-})
+
+if (document.readyState ==='loading') {
+    document.addEventListener('DOMContentLoaded',ready());
+    
+} else{
+    ready();
+    
+}
+function ready(){
+    let overlays= Array.from(document.getElementsByClassName('overlay-text'));
+    let cards=Array.from(document.getElementsByClassName('card'));
+   
+   
+   
+   
+     overlays.forEach(overLay =>{
+          overlay.addEventListener('click',()=>{
+            overlay.classList.remove('visible');
+        });
+    });
+    cards.forEach(Card =>{
+        card.addEventListener('click',()=>{
+            // game.flipCard(card);
+        });
+    });
+}
+     
