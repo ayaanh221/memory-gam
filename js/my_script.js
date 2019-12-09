41840f55361c8d7b68b9e73f22e10516ff08b9b1
ayaan <ayaanh221@gmail.com>
2019-12-09 13:26:05 +0000

    first commit

--- js/my_script.js
+++ js/my_script.js
@@ -1,27 +1,85 @@
 
-if (document.readyState ==='loading') {
-    document.addEventListener('DOMContentLoaded',ready());
+
+const cards = document.querySelectorAll('.card');
+
+
+let hasFlippedCard =false;
+let lockBoard = false;
+let firstCard,secondCard; 
+
+
+function flipCard(){
+    if(lockBoard) return;
+    if(this===firstCard)return;
+    this.classList.add('visible');
+ 
+        
+  
+    if(!hasFlippedCard){
+    //first click
+    hasFlippedCard= true;
+    firstCard=this;
+   return ;
+} 
+    //second click
+    hasFlippedCard=false;
+    secondCard=this;
     
-} else{
-    ready();
+   
+     checkForMatch();
+
+}
+
+function checkForMatch(){
+  let isMatch =  firstCard.dataset.framework ===
+   secondCard.dataset.framework;
+   isMatch ? disablCards() : unflipCards();
+   
+   
+function disablCards(){
+     firstCard.removeEventListener('click', flipCard);
+       secondCard.removeEventListener('click',flipCard);
+        resetBoard();
+}
+function unflipCards(){
+    lockBoard= true;
+    setTimeout(()=>{
+        firstCard.classList.remove('visible');
+    secondCard.classList.remove('visible');
+    lockBoard =false;
+    resetBoard();
+},1500);
+    } 
+}
+function resetBoard(){
+    [hasFlippedCard,lockBoard]=[false,false];
+    [firstCard,secondCard]=[null,null];
+}
+
+
+(function shuffle(){
+    cards.forEach(card=>{
+        let randomPos = Math.floor(Math.random() *12);
+        card.style.order= randomPos;
+    });
+})();
+
+function play() {
+        var audio = document.getElementById('audio');
+        if (audio.paused) {
+            audio.play();
+            ('#play').removeClass('glyphicon-play-circle');
+            ('#play').addClass('glyphicon-pause');
+        }else{
+            audio.pause();
+            audio.currentTime = 0;
+            ('assets/audio/game background music.mp3').addClass('glyphicon-play-circle');
+            ('#play').removeClass('glyphicon-pause');
+        }
+    }
+
+
+
     
-}
-function ready(){
-    let overlays= Array.from(document.getElementsByClassName('overlay-text'));
-    let cards=Array.from(document.getElementsByClassName('card'));
-   
-   
-   
-   
-     overlays.forEach(overLay =>{
-          overlay.addEventListener('click',()=>{
-            overlay.classList.remove('visible');
-        });
-    });
-    cards.forEach(Card =>{
-        card.addEventListener('click',()=>{
-            // game.flipCard(card);
-        });
-    });
-}
-     
+    
+    cards.forEach(card => card.addEventListener('click',flipCard));
